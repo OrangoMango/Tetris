ec414baf45b6f43eeceb9cc60e56da32a383b0aa
OrangoMango <orapama06@gmail.com>
2023-09-19 14:13:40 +0200

    Added project directory

---
+++ src/main/java/com/orangomango/tetris/MainApplication.java
@@ -0,0 +1,43 @@
+package com.orangomango.tetris;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.canvas.*;
+import javafx.scene.paint.Color;
+import javafx.animation.*;
+import javafx.util.Duration;
+
+public class MainApplication extends Application{
+	private static final double WIDTH = 600;
+	private static final double HEIGHT = 600;
+	private static final int FPS = 40;
+	
+	@Override
+	public void start(Stage stage){
+		StackPane pane = new StackPane();
+		Canvas canvas = new Canvas(WIDTH, HEIGHT);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		pane.getChildren().add(canvas);
+		
+		Timeline loop = new Timeline(new KeyFrame(Duration.millis(1000.0/FPS), e -> update(gc)));
+		loop.setCycleCount(Animation.INDEFINITE);
+		loop.play();
+
+		stage.setTitle("Tetris");
+		stage.setResizable(false);
+		stage.setScene(new Scene(pane, WIDTH, HEIGHT));
+		stage.show();
+	}
+	
+	private void update(GraphicsContext gc){
+		gc.clearRect(0, 0, WIDTH, HEIGHT);
+		gc.setFill(Color.BLACK);
+		gc.fillRect(0, 0, WIDTH, HEIGHT);
+	}
+	
+	public static void main(String[] args){
+		launch(args);
+	}
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.orangomango.tetris{
+	requires javafx.controls;
+	requires javafx.media;
+
+	exports com.orangomango.tetris;
+}
